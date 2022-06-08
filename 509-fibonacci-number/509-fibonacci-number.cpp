9a9daec1c948a52e8ea45ba5b6f8c033da3c4c9c
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2022-06-08 22:30:21 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (10.00%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -5,9 +5,8 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> v(n+2);
-     // v.push_back(0);
-     //    v.push_back(1);
+        if(n<2) return n;
+        vector<int> v(n+1);
   v[0]=0;
         v[1]=1;
         for(int i=2;i<=n;i++){
