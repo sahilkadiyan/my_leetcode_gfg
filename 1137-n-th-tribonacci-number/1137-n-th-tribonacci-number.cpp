ac3b5eab197a1984b12b812f056e4309659ca37b
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2022-06-08 23:32:34 +0530

    Time: 5 ms (9.12%), Space: 5.8 MB (83.42%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+//dp tabulation method
+class Solution {
+public:
+    int tribonacci(int n) {
+        //base case
+        int mem[n+1];
+       
+        if (n < 2) 
+            return n;
+        if(n==2) return n-1; 
+        mem[0]=0,mem[1]=1,mem[2]=1;
+        for(int i=3;i<=n;i++){
+            mem[i]=mem[i-1]+mem[i-2]+mem[i-3];
+        }
+        return mem[n];
+        
+      
+    }
+};
