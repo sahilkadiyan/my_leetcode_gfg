3ace3a4fd49066689c9b2aa064328959ebda3c0e
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2022-06-08 23:32:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.42%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -7,7 +7,7 @@ public:
        
         if (n < 2) 
             return n;
-        if(n==2) return n-1; 
+        // if(n==2) return n-1; 
         mem[0]=0,mem[1]=1,mem[2]=1;
         for(int i=3;i<=n;i++){
             mem[i]=mem[i-1]+mem[i-2]+mem[i-3];
