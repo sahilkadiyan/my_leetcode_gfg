1eaac1c2645d7ae2085e975fc925a06d073088ab
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2022-06-08 22:16:47 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -9,7 +9,7 @@ class Solution {
   public:
     long long int nthFibonacci(long long int n){
         // code here
-           long long int mem[n+2];
+           long long int mem[n+1];
         mem[0]=0,mem[1]=1;
         for(long long int  i=2;i<=n;i++){
             mem[i]=(mem[i-1]+mem[i-2])%1000000007;
