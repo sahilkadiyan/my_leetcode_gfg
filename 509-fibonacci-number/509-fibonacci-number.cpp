73477326d0f65ddda78faeeb4eba0a26b7035231
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2022-06-08 21:41:32 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.97%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+//this question can be done in 3 ways:
+//1: recursion  2: memoization  3: tabulation method
+//this one is the tabulation method tabulation is a bottom up appraoch in which we know the result of the previous term and with the help of that we can compute the next term easily 
+//here we know the 1st two terms of fibonacci series i.e 0 and 1 so with the help of these two numbers we can easily compute the next term 
+class Solution {
+public:
+    int fib(int n) {
+        int mem[n+2];
+        mem[0]=0,mem[1]=1;
+        for(int i=2;i<=n;i++){
+            mem[i]=mem[i-1]+mem[i-2];
+        }
+        return  mem[n];
+       
+    }
+};
