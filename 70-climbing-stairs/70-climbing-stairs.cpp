fa67f9a8e9c1868a7aef3858742cb3ad2bd0cde1
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2022-07-19 15:04:10 +0530

    Time: 3 ms (35.83%), Space: 5.8 MB (83.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    //here we need to remember that at 0th stair we can take 1 step i.e for n==1 that's why we take prev2=1 
+    //space : o(1)
+    //time : o(n)
+    
     int climbStairs(int n) {
         
-        int prev2=1;
+        int prev2=1;  //The base case will be when we want to go to the 0th stair, then we have only one option.
+        
         int prev=1;
+        
         for(int i=2;i<=n;i++){
-            int curri=prev+prev2;
+            int curri=(prev+prev2);
             prev2=prev;
             prev=curri;
         }
