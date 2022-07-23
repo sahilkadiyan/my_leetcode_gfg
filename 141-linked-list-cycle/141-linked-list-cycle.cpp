74f43b7414dcefeef1e313971bbc75b89806898d
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2022-07-23 11:59:16 +0530

    Time: 35 ms (7.08%), Space: 10.5 MB (12.86%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,15 +9,12 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        
-          if(head==NULL) return false;
-    ListNode* walker = head;
-    ListNode *runner = head;
-    while(runner->next!=NULL && runner->next->next!=NULL) {
-        walker = walker->next;
-        runner = runner->next->next;
-        if(walker==runner) return true;
-    }
-    return false;
+        unordered_set<ListNode*> s;
+        for(ListNode* temp=head;temp!=NULL;temp=temp->next){
+            if(s.find(temp)!=s.end()) return true;
+            s.insert(temp);
+            
+        }
+        return false;
     }
 };
