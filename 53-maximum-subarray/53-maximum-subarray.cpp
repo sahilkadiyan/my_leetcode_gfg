3229cf10ac35b53a1538f63763875a8961861f6e
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2022-08-01 02:38:22 +0530

    Time: 198 ms (31.35%), Space: 67.8 MB (53.98%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,19 +1,13 @@
-//in this question we are just traversing the whole vector and try to find out the maximum sum of the subarray which is continuous 
-//time complexity: o(n)
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        int max_sofar=INT_MIN;
-        int maxendinghere=0;
-        for(int i=0;i<nums.size();i++){
-            maxendinghere=maxendinghere+nums[i];
-            if(max_sofar<maxendinghere){
-                max_sofar=maxendinghere;
-            }
-            if(maxendinghere<0)
-                maxendinghere=0;
+        int ans=nums[0]; //initially our result is 1st element of the array 
+        int maxending=nums[0]; //prev sum of the subarray
+        for(int i=1;i<nums.size();i++){
+            maxending=max(maxending+nums[i],nums[i]);
+            ans=max(ans,maxending);
         }
-        return max_sofar;
+        return ans;
     }
 };
