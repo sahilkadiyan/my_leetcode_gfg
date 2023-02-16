aad4fc5b41ea4a38b1b371e383949a5bb968f415
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2023-02-16 15:53:00 +0530

    Time: 8 ms (76.88%), Space: 18.8 MB (63.94%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+           if(root==NULL) return 0;
+       int x=0;
+        int y=0;
+        x=maxDepth(root->left);
+        y=maxDepth(root->right);
+        if(x>y) return 1+x;
+        return 1+y;
+    }
+};
