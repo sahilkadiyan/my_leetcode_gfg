31a885aaaeb3cf303d1f6c1c0c3869a426bde45d
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2023-06-02 18:34:00 +0530

    Time: 252 ms (18.94%), Space: 43.9 MB (12.32%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maximumDetonation(int[][] bombs) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        int n = bombs.length;
+
+        // Build the graph
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
+                int xj = bombs[j][0], yj = bombs[j][1];
+
+                // Create a path from node i to node j, if bomb i detonates bomb j.
+                if ((long)ri * ri >= (long)(xi - xj) * (xi - xj) + (long)(yi - yj) * (yi - yj)) {
+                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int count = dfs(i, new HashSet<>(), graph);
+            answer = Math.max(answer, count);
+        }
+
+        return answer;
+    }
+
+    // DFS to get the number of nodes reachable from a given node cur
+    private int dfs(int cur, Set<Integer> visited, Map<Integer, List<Integer>> graph) {
+        visited.add(cur);
+        int count = 1;
+        for (int neib : graph.getOrDefault(cur, new ArrayList<>())) {
+            if (!visited.contains(neib)) {
+                count += dfs(neib, visited, graph);
+            }
+        }
+        return count;
+    }
+}
