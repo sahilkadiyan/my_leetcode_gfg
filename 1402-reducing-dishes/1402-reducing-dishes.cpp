3b5d1ce4300137b92620f2c226f6d17269bf4420
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2023-08-21 21:34:30 +0530

    Time: 41 ms (41.26%), Space: 21 MB (12.28%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int findMaxSatisfaction(vector<int>& satisfaction, vector<vector<int>>& memo, int index, int time) {
-        // Return 0 if we have iterated over all the dishes.
-        if (index == satisfaction.size()) {
+    int solve(vector<int>& satisfaction,int index,int time){
+        if(index==satisfaction.size()){
             return 0;
         }
+        int include=satisfaction[index]*(time+1) + solve(satisfaction,index+1,time+1);
+        int exclude=0+solve(satisfaction,index+1,time);
+        return max(include,exclude);
         
-        // We have already calculated the answer, so no need to go into recursion.
-        if (memo[index][time] != -1) {
-            return memo[index][time];
-        }
-        
-        // Return the maximum of two choices:
-        // 1. Cook the dish at `index` with the time taken as `time` and move on to the next dish with time as time + 1.
-        // 2. Skip the current dish and move on to the next dish at the same time.
-        return memo[index][time] = max(satisfaction[index] * time + findMaxSatisfaction(satisfaction, memo, index + 1, time + 1), 
-                                       findMaxSatisfaction(satisfaction, memo, index + 1, time));
     }
     
-    int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.begin(), satisfaction.end());
-
-        // Mark, all the states as -1, denoting not yet calculated.
-        vector<vector<int>> memo(satisfaction.size() + 1, vector<int>(satisfaction.size() + 1, -1));
+      int solveMem(vector<int>& satisfaction,int index,int time,vector<vector<int>> &dp){
+        if(index==satisfaction.size()){
+            return 0;
+        }
+          if(dp[index][time]!=-1) return dp[index][time];
+        int include=satisfaction[index]*(time+1) + solveMem(satisfaction,index+1,time+1,dp);
+        int exclude=0+solveMem(satisfaction,index+1,time,dp);
+        return dp[index][time]=max(include,exclude);
         
-        return findMaxSatisfaction(satisfaction, memo, 0, 1);
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(),satisfaction.end());
+        
+        int n=satisfaction.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return solve(satisfaction,0,0);
+        return solveMem(satisfaction,0,0,dp);
+        
     }
 };
