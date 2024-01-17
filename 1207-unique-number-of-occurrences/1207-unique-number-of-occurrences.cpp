77990e067d6fceb20dd7e678965a0af457646adc
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-01-17 10:08:53 +0530

    Time: 2 ms (65.84%), Space: 8.6 MB (69.36%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -4,13 +4,17 @@ public:
         
          unordered_map<int,int>mp;
         set<int>ans;
+        
+        //storing elements in the map with their frequency
       for(int i=0;i<arr.size();i++){
         mp[arr[i]]++;
       }
-      
+        
+      //insert the elements in the set with their frequency from the map
       for(auto i=mp.begin();i!=mp.end();i++){
         ans.insert(i->second);
       }
+        
       return ans.size()==mp.size() ?true:false;
         
       
