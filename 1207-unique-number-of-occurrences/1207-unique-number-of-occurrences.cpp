38dcda2616bc6fb8f5d693c5a9f38fce490ec3e8
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-01-17 10:01:39 +0530

    Time: 5 ms (39.16%), Space: 8.7 MB (41.08%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        
+         unordered_map<int,int>mp;
+        set<int>ans;
+      for(int i=0;i<arr.size();i++){
+        mp[arr[i]]++;
+      }
+      
+      for(auto i=mp.begin();i!=mp.end();i++){
+        ans.insert(i->second);
+      }
+      return ans.size()==mp.size() ?true:false;
+        
+      
+        
+    }
+};
