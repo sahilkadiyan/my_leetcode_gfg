22f3e483b3e00e202a6d5843ce1711b287a1709e
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-01-24 23:01:34 +0530

    Time: 241 ms (93.03%), Space: 178.4 MB (96.51%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,39 +1,14 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-   	void pseudoCount(TreeNode*root,vector<int>&v_count,int& count){
-    if(root==NULL) return;
-    v_count[root->val]++;
-    pseudoCount(root->left,v_count,count);
-    pseudoCount(root->right,v_count,count);
-    
-    if(root->left==NULL && root->right==NULL){
-        int flag=0;
-        for(int i=1;i<=9;i++){
-            if(v_count[i]%2!=0){
-                flag++;
-            }
-        }
-        if(flag==1||flag==0){
-            count++;
-        }   
+    int solve(TreeNode* root, int count ) {
+        if (!root) return 0;
+        count ^= 1 << (root->val - 1);
+        int res = solve(root->left, count) + solve(root->right, count);
+        if (root->left == root->right && (count & (count - 1)) == 0) res++;
+        return res;
     }
-    v_count[root->val]--;
-}
-int pseudoPalindromicPaths (TreeNode* root) {
-    int count=0;
-    vector<int> v_count(10,0);
-    pseudoCount(root,v_count,count);
-    return count;
-}
+    int pseudoPalindromicPaths (TreeNode* root) {
+        return solve(root,0);
+        
+    }
 };
