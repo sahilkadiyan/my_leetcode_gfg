f87625b47e5f47fe31234dd5ddc130affb49c3e3
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-01-25 16:53:57 +0530

    Time: 11 ms (70.12%), Space: 14.1 MB (5.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,9 +4,12 @@ public:
         vector<int> v;
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
+             
             if(mp.find(target-nums[i])!=mp.end()){
+                
                 v.push_back(mp[target-nums[i]]);
                 v.push_back(i);
+                
                 return v;
             }
             mp[nums[i]]=i;
