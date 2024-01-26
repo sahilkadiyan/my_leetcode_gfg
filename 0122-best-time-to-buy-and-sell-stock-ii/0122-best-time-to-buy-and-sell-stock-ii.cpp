92f7c3fa7f84a69b1cf6437a45aefa32d9d326fe
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-01-26 16:09:29 +0530

    Time: 3 ms (90.57%), Space: 15.5 MB (21.92%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-         int profit = 0;
-        for(int i = 1; i < size(prices); i++)
-            if(prices[i] > prices[i-1])              // yesterday was valley, today is peak
-                profit += prices[i] - prices[i-1];   // buy yesterday, sell today
-        return profit;
+        int profit=0;
         
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>prices[i-1]){
+                profit+=(prices[i]-prices[i-1]);
+            }
+        }
+        return profit;
     }
 };
