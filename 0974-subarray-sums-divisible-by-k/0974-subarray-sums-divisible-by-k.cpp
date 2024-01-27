44187594a4983b95efdcdcaf6ff0f46b816fd1b3
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-01-27 23:14:00 +0530

    Time: 36 ms (80.75%), Space: 35.2 MB (5.05%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
-    int subarraysDivByK(std::vector<int>& nums, int k) {
-        int ans = 0;
-
-        std::unordered_map<int, int> map;
-        map[0] = 1;
-
-        int sum = 0;
-        int rem = 0;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            sum += nums[i];
-            rem = sum % k;
-
-            if (rem < 0) {
-                rem += k;
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int ans=0;   //count the number of suybarrays 
+        
+        unordered_map<int ,int> map;
+        map[0]=1;  //this 1 is the frequencey of the remainder which is 0 initially 
+        int sum=0;
+        int rem=0;
+        
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            rem=sum%k;
+            
+            if(rem<0){
+                rem+=k;
             }
-
-            if (map.find(rem) != map.end()) {
-                ans += map[rem];
-                map[rem]++;
-            } else {
-                map[rem] = 1;
+            
+            if(map.find(rem)!=map.end()){
+                ans+=map[rem];  //stroing the freq in the final result 
+                map[rem]++;   //incrementing the frequency of the remainder in the map
+            }
+            else{
+                map[rem]=1;
             }
         }
-
+        
         return ans;
+        
+        
+        
+        
+        
+      
     }
 };
