dbd9c7b8105660c54a8db7e087e1843599e5c685
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-01-29 16:09:38 +0530

    Time: 70 ms (56.18%), Space: 61.3 MB (9.52%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
-        //we will have two pointers one is at extreme left and the other one is at the right 
-        //now we try to reduce the width of the container 
-        
         int left=0;
         int right=height.size()-1;
-        
         int maximumArea=0;
-        while(left<right){
+        while(left<=right){
             
             int area=min(height[left],height[right])*(right-left);
             maximumArea=max(area,maximumArea);
-            
             if(height[left]<height[right]) left++;
             else right--;
         }
         
         return maximumArea;
-        
         
     }
 };
