562bbd2253033407fbe2bb1a16e6477b8b810965
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-02-02 15:16:11 +0530

    Time: 56 ms (93.43%), Space: 45.1 MB (43.71%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
    int subarraySum(vector<int>& nums, int k) {
        int ans = 0;
-       unordered_map<int, int> prefixSumCount;  // Use unordered_map for efficiency
-       prefixSumCount[0] = 1;  // Initialize for empty subarray
-       int currentSum = 0;
+       unordered_map<int, int> map;  // Use unordered_map for efficiency
+       map[0] = 1;  // Initialize for empty subarray
+       int sum = 0;
 
        for (int num : nums) {
-           currentSum += num;
-           int complement = currentSum - k;  // Find the complement to reach target sum
-           if (prefixSumCount.count(complement)) {
-               ans += prefixSumCount[complement];
+           sum += num;
+           if (map.count(sum-k)) {
+               ans += map[sum-k];
            }
-           prefixSumCount[currentSum]++;  // Increment count for current prefix sum
+           map[sum]++;  // Increment count for current prefix sum
        }
 
        return ans;
