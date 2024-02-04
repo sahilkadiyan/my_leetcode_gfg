1c277c9ff21df60fa38a9d1cbc4277f61b068a64
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-02-04 12:57:42 +0530

    Time: 19 ms (42.24%), Space: 10.3 MB (15.00%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-          vector<int> map(128,0);
-        for (char c : t) {
-            map[c]++;
-        }
+   string minWindow(string s, string t) {
 
-        int counter = t.size(), begin = 0, end = 0, d = INT_MAX, head = 0;
-        while (end < s.size()){
-            if (map[s[end++]]-- > 0) {
-                counter--;
+    // Store the counts of characters in 't'
+    unordered_map<char, int> letters;
+    for(auto c : t) letters[c]++; 
+
+    int count = 0; // Number of valid letters in the current window
+    int low = 0; // Stores the position of the first character in window
+    // Stores location and length of best substring
+    int min_length = INT_MAX, min_start = 0;
+    
+    // Iterate over 's'
+    for(int high = 0; high<s.length(); high++) {
+        // If this character is required, then update count (Add it to the window)
+        if(letters[s[high]] > 0) count++;
+        // Reduce the count for this character (since we have added this to the window)
+        letters[s[high]]--;
+        // If we have all the valid characters, update substring
+        if(count == t.length()) {
+            // What exactly are we doing in the loop below?
+            //  This piece of code basically makes sure that letters[s[low]] is not negative
+            //  Because if it is negative, than that means we have more s[low]s than required.
+            //  So we have to remove such characters
+            while(low < high && letters[s[low]] < 0){ 
+                letters[s[low]]++; // Remove character from window, update count
+                low++; // Update substring window start
             }
-            while (counter == 0) {
-                if (end - begin < d) {
-                    head = begin;
-                    d = end - head;
-                }
-                if (map[s[begin++]]++ == 0) {
-                    counter++;
-                }
-            }  
+            // Update substring
+            if(min_length > (high - low + 1)){
+                min_start = low; // Set start
+                min_length = high - low + 1; // Set length
+            }
         }
-        return d == INT_MAX ? "" : s.substr(head, d);
     }
+
+    // No substring satisfies 't'
+    if(min_length == INT_MAX) return "";
+
+    // Return the best substring
+    return s.substr(min_start, min_length);
+}
 };
