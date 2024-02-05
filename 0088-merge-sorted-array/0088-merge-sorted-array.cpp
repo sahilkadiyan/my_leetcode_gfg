c081b23980eab5ed9c47ee997a69fe972b0b49e6
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-02-05 21:21:30 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (9.21%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int last=m+n-1;
+        
+        //traversing in the reverse order
+        
+        while(m>0 && n>0){
+            if(nums1[m-1]>nums2[n-1]){
+                nums1[last]=nums1[m-1];
+                m--;
+            }
+            else {
+                nums1[last]=nums2[n-1];
+                n--;
+            }
+            last--;
+        }
+        //fill nums1 with the reamining elements of nums2
+        while(n>0){
+            nums1[last]=nums2[n-1];
+            n--;
+            last--;
+        }
+    }
+};
