37db4a8c07fc9eed27ab8c0f7e991f7701783cb1
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-02-09 22:22:57 +0530

    Time: 3 ms (41.87%), Space: 8.2 MB (23.60%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+
+class Solution {
+public:
+    int countNeighbours(std::vector<std::vector<int>>& board, int r, int c, int rows, int cols) {
+        int cnt = 0;
+        for (int i = r - 1; i < r + 2; i++) {
+            for (int j = c - 1; j < c + 2; j++) {
+                if ((i == r && j == c) || i < 0 || j < 0 || i >= rows || j >= cols)
+                    continue;
+                if (board[i][j] == 1 || board[i][j] == 3)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    void gameOfLife(std::vector<std::vector<int>>& board) {
+        int rows = board.size(), cols = board[0].size();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int cnt = countNeighbours(board, i, j, rows, cols);
+
+                if (board[i][j] >= 1 && (cnt == 2 || cnt == 3))
+                    board[i][j] = 3;
+                else if (cnt == 3)
+                    board[i][j] = 2;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 1)
+                    board[i][j] = 0;
+                else if (board[i][j] >= 2)
+                    board[i][j] = 1;
+            }
+        }
+    }
+};
