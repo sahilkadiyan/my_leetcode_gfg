f6a41a9b97b7166f975012cf678dfbd6621016ce
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-02-11 01:58:37 +0530

    Time: 207 ms (79.24%), Space: 95.8 MB (37.74%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int,int>>p;
+        priority_queue<pair<int,int>> p;
+        //1st value is xi-yi and 2nd value is xi
+        
         int ans=INT_MIN;
         for(int i=0;i<points.size();i++){
-			// if  xj - xi > k then we have to remove that from the queue as it can not longer 
-			// contribute to the answer which is given in the question itself
-            while(!p.empty() && (points[i][0]-p.top().second)>k){
-                p.pop();
-            }
-			
-			// now if the queue is not empty then the top of the queue is having 
-			// maximum value of  ( yi-xi) so we update our answer with  (top of the queue) + currvalue
+            
+            //if xj-xi> k 
+            while(!p.empty() && (points[i][0]-p.top().second)>k) p.pop();
+            
             if(!p.empty()){
                 ans=max(ans,points[i][0]+points[i][1]+p.top().first);
             }
-			
-			// inserting ( yi-xi ) into the queue 
+            
             p.push({points[i][1]-points[i][0],points[i][0]});
         }
+        
         return ans;
     }
 };
