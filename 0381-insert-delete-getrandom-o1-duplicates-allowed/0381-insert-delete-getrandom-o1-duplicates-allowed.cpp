2836b7155c5674853fd4abeedf1c21e97facf354
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-02-12 01:47:26 +0530

    Time: 182 ms (72.87%), Space: 112.8 MB (5.43%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,34 +1,49 @@
 class RandomizedCollection {
-private:
+    
+    private:
     vector<int> lst;
-    unordered_map<int, unordered_set<int>> idx;
+    unordered_map<int,unordered_set<int>> idx;
     default_random_engine rand;
-
 public:
-    RandomizedCollection() : rand(random_device{}()) {}
-
-    bool insert(int val) {
-        if (!idx.count(val)) idx[val] = unordered_set<int>();
-        idx[val].insert(lst.size());
-        lst.push_back(val);
-        return idx[val].size() == 1;
+    RandomizedCollection() :rand(random_device{}()) {
+        
     }
-
+    
+    bool insert(int val) {
+        if(!idx.count(val)) idx[val]=unordered_set<int>();
+        idx[val].insert(lst.size());
+        
+        lst.push_back(val);
+        
+        return idx[val].size()==1;
+        
+    }
+    
     bool remove(int val) {
-        if (!idx.count(val) || idx[val].size() == 0) return false;
-        int removeIdx = *idx[val].begin();
+        if(!idx.count(val) || idx[val].size()==0) return false;
+        int removeIdx= *idx[val].begin();
         idx[val].erase(removeIdx);
-        int last = lst.back();
-        lst[removeIdx] = last;
+        int last=lst.back();
+        lst[removeIdx]=last;
         idx[last].insert(removeIdx);
-        idx[last].erase(lst.size() - 1);
-
+        idx[last].erase(lst.size()-1);
+        
         lst.pop_back();
         return true;
+        
     }
-
+    
     int getRandom() {
-        uniform_int_distribution<int> dist(0, lst.size() - 1);
+        uniform_int_distribution<int> dist(0,lst.size()-1);
         return lst[dist(rand)];
+        
     }
 };
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
