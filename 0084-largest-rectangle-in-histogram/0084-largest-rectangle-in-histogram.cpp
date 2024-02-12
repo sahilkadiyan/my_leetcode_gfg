8d2f7e300ca4f4be2f32d863a04850c559a770f7
Sahil Kadiyan <74564606+sahilkadiyan@users.noreply.github.com>
2024-02-13 01:04:09 +0530

    Time: 114 ms (82.93%), Space: 78.7 MB (72.29%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int maxArea = 0;
-        stack<pair<int, int>> s; // pair : (index, height)
-
-        for (int i = 0; i < heights.size(); ++i) {
-            int start = i;
-            while (!s.empty() && s.top().second > heights[i]) {
-                int index = s.top().first;
-                int height = s.top().second;
-                s.pop();
-                maxArea = max(maxArea, height * (i - index));
-                start = index;
+        int n=heights.size();
+        stack<int> st;
+        int leftSmall[n],rightSmall[n];
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
             }
-            s.push(make_pair(start, heights[i]));
+            if(st.empty()) leftSmall[i]=0;
+            else leftSmall[i]=st.top()+1;
+            st.push(i);
         }
-
-        while (!s.empty()) {
-            int index = s.top().first;
-            int height = s.top().second;
-            s.pop();
-            maxArea = max(maxArea, height * (int)(heights.size() - index));
+        
+        while(!st.empty()) st.pop();
+        
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            if(st.empty()) rightSmall[i]=n-1;
+            else rightSmall[i]=st.top()-1;
+            st.push(i);
         }
-        return maxArea;
+        
+        int maxA=0;
+        for(int i=0;i<n;i++){
+            maxA=max(maxA,heights[i]*(rightSmall[i]-leftSmall[i]+1));
+        }
+        
+        return maxA;
+        
+        
     }
 };
